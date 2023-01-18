edac20d75696e0bd86af7dc9245697e58e043157
Harshal Sheth <hsheth2@gmail.com>
2023-01-17 18:47:06 -0800

    add ref to customer_snapshot

--- models/transform/customer_details.sql
+++ models/transform/customer_details.sql
@@ -5,6 +5,12 @@
 SELECT
     c.customer_id,
     c.first_name || ' ' || c.last_name as "full_name",
+    (
+        select cs.first_name || ' ' || cs.last_name
+        from {{ ref('customer_snapshot') }} cs where cs.customer_id = c.customer_id
+        order by dbt_valid_from desc
+        limit 1
+    ) as "initial_full_name",
     c.email,
     a.address,
     m.city,
